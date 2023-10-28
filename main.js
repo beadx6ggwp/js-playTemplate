e742a514da5b4c03bb11329b6d1e27f767aab630
beadx6 <davidmd9830415@gmail.com>
2023-10-28 08:39:15 +0800

    upload

---
+++ main.js
@@ -0,0 +1,25 @@
+/* 
+運作流程: 
+    資源載入完成後自動調用windows.onload()->
+    init()->mainloop-> [upadte->draw]反覆循環
+    requestAnimationFrame為類似自動垂直同步的timer
+keys: 
+    按鍵被按下為true,按下a後keys[65]為true,詳細可看console.log(keys);
+mousePos:
+    滑鼠當前位置 mousePos.x,mousePox.y 即可存取
+*/
+
+function init() {
+    console.log("Start");
+
+}
+
+// loop star
+function update(dt) {
+
+}
+
+
+function draw(ctx) {
+
+}
